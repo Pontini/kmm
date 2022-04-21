78b27beb0dff66067623d8dbbca9deb2894d68c3
ewerton-pontini <ewerton.pontini@picpay.com>
2022-04-21 14:11:12 -0300

    Add Ktor library dependency

---
+++ MyEarnings/shared/src/commonMain/kotlin/app/systems/myearnings/impl/external/remote/StockClient.kt
@@ -0,0 +1,31 @@
+package app.systems.myearnings.impl.external.remote
+
+import app.systems.myearnings.sample.Greeting
+import io.ktor.client.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+
+class StockClient {
+    private val client = HttpClient()
+
+    private suspend fun getHtml(): String {
+        val response = client.get("https://ktor.io/docs")
+        return response.bodyAsText()
+    }
+
+    fun main() {
+        val mainScope = MainScope()
+        mainScope.launch {
+            kotlin.runCatching {
+                getHtml()
+            }.onSuccess {
+                print(it)
+            }.onFailure {
+                print(it.message)
+            }
+        }
+
+    }
+}
