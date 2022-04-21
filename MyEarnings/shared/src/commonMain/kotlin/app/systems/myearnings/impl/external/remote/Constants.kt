c154ac9671c5e033b2841689bca49019323d02e4
ewerton-pontini <ewerton.pontini@picpay.com>
2022-04-21 16:10:17 -0300

    Add sample API

---
+++ MyEarnings/shared/src/commonMain/kotlin/app/systems/myearnings/impl/external/remote/Constants.kt
@@ -0,0 +1,15 @@
+package app.systems.myearnings.impl.external.remote
+
+
+const val API_KEY = "O42CEXF2N4E4KQWY"
+
+
+
+/*
+   SAMPLE CALL API
+   @GET("/query")
+    suspend fun searchForSymbol(@Query("keywords") keywords: String, @Query("apikey") apikey: String="O42CEXF2N4E4KQWY",@Query("function") function: String="SYMBOL_SEARCH"): ResponseBody
+
+    @GET("/query")
+    suspend fun detailsFavoriteStock(@Query("symbol") symbol: String, @Query("apikey") apikey: String="O42CEXF2N4E4KQWY",@Query("function") function: String="TIME_SERIES_DAILY"): ResponseBody
+*/
